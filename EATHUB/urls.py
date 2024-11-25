b8c5009ba6deb0e2c38526346b8850a7df2070dc
prashant <prashantpatle0602@gmail.com>
2024-11-25 07:14:26 +0000

    start project

--- EATHUB/urls.py
+++ EATHUB/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('accounts/', include('allauth.urls')),
+    # path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('', include('Restaurants.urls')),
